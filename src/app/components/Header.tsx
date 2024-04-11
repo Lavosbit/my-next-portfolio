5dfced7820ef3a59813652eda9c9dbb6ea4b4aec
Jacob Galito <jacob@lavosbit.com>
2024-04-10 20:08:46 -0400

    sadfasdf

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="flex fixed top-6 rounded-full left-1/2 -translate-x-1/2 w-[800px] backdrop-blur-md py-4 px-4 md:px-12 justify-between bg-white shadow-xl shadow-blue-300/15">
-      <Link href="/" className="font-display">
-        Jacob Galito | Senior Designer
-      </Link>
+      <h1>
+        <Link href="/">Jacob Galito | Senior Designer</Link>
+      </h1>
 
       <nav className="flex space-x-4">
         <Link href="/">Home</Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,6 @@
 import type { Config } from "tailwindcss";
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: ["./src/app/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {
-        display: ["Georgia", ...defaultTheme.fontFamily.serif],
-      },
-    },
-  },
 };
 export default config;
